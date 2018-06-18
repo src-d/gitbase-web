b11d926194647110a4f63412e62eb4017633bc6c
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-06-18 17:40:55 +0200

    Add support for svg-react-loader
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,4 +1,5 @@
 const { compose } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
+const rewireSvgReactLoader = require('react-app-rewire-svg-react-loader');
 
-module.exports = compose(rewireLess);
+module.exports = compose(rewireLess, rewireSvgReactLoader);
