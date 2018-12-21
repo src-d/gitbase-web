6054671b832e9fc7b8167d64a5ac7d66b758ae8a
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-12-21 12:12:30 +0000

    Add keyboard shortcuts modal help
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

---
+++ frontend/src/components/HelpModal.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import CloseIcon from '../icons/close-query-tab.svg';
+import './HelpModal.less';
+
+class HelpModal extends Component {
+  render() {
+    const { showModal, onHide } = this.props;
+
+    return (
+      <Modal show={showModal} onHide={onHide}>
+        <Modal.Header>
+          <Modal.Title>
+            Help
+            <CloseIcon className="btn-modal-close" onClick={onHide} />
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            For a reference of gitbase SQL, please read{' '}
+            <strong>
+              <a href="https://docs.sourced.tech/gitbase">
+                docs.sourced.tech/gitbase
+              </a>
+            </strong>
+          </div>
+          <h4 className="kb-shortcuts-title">Keyboard shortcuts</h4>
+          <table className="keyboard-shortcuts">
+            <tbody>
+              <tr>
+                <td className="keys">
+                  <div className="key">Ctrl</div>
+                  &nbsp;+&nbsp;
+                  <div className="key">Enter</div>
+                </td>
+                <td>Run the query</td>
+              </tr>
+              <tr>
+                <td className="keys">
+                  <div className="key">Ctrl</div>
+                  &nbsp;+&nbsp;
+                  <div className="key">Space</div>
+                </td>
+                <td>Autocomplete</td>
+              </tr>
+            </tbody>
+          </table>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
+
+HelpModal.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired
+};
+
+export default HelpModal;
