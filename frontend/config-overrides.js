9667cde44cb8961055d57778b6a8e8da7883f66d
Carlos Martín <carlos.martin.sanchez@gmail.com>
2019-04-05 12:27:35 +0100

    Remove react-app-rewired
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- frontend/config-overrides.js
+++
@@ -1,5 +0,0 @@
-const { compose } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-const rewireSvgReactLoader = require('react-app-rewire-svg-react-loader');
-
-module.exports = compose(rewireLess, rewireSvgReactLoader);
